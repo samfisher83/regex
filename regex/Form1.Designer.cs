61ed7d50507a8fbb552831a69431ffab36c05d33
none <none>
2014-01-02 11:04:18 -0600

    Regex
    A simple windows program to do regex operations

---
+++ regex/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            //this.inputText = new System.Windows.Forms.TextBox();
+            this.inputText = new MyTextBox();
+            this.inputSeach = new System.Windows.Forms.TextBox();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.outputString = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(817, 430);
+            this.splitContainer1.SplitterDistance = 35;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(84, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Line Number";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.inputText);
+            this.splitContainer2.Panel1.Controls.Add(this.inputSeach);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.outputText);
+            this.splitContainer2.Panel2.Controls.Add(this.outputString);
+            this.splitContainer2.Size = new System.Drawing.Size(817, 391);
+            this.splitContainer2.SplitterDistance = 411;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // inputText
+            // 
+            this.inputText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputText.Location = new System.Drawing.Point(3, 29);
+            this.inputText.MaxLength = 10000000;
+            this.inputText.Multiline = true;
+            this.inputText.Name = "inputText";
+            this.inputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.inputText.Size = new System.Drawing.Size(395, 350);
+            this.inputText.TabIndex = 1;
+            // 
+            // inputSeach
+            // 
+            this.inputSeach.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputSeach.Location = new System.Drawing.Point(3, 3);
+            this.inputSeach.Name = "inputSeach";
+            this.inputSeach.Size = new System.Drawing.Size(395, 20);
+            this.inputSeach.TabIndex = 0;
+            // 
+            // outputText
+            // 
+            this.outputText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputText.Location = new System.Drawing.Point(3, 29);
+            this.outputText.MaxLength = 10000000;
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputText.Size = new System.Drawing.Size(387, 350);
+            this.outputText.TabIndex = 1;
+            // 
+            // outputString
+            // 
+            this.outputString.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputString.Location = new System.Drawing.Point(3, 3);
+            this.outputString.Name = "outputString";
+            this.outputString.Size = new System.Drawing.Size(387, 20);
+            this.outputString.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 430);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Regex";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        //private System.Windows.Forms.TextBox inputText;
+        private MyTextBox inputText;
+        private System.Windows.Forms.TextBox inputSeach;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.TextBox outputString;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ regex/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            inputText.Pasted += new EventHandler<ClipboardEventArgs>(inputText_Pasted);
+            
+        }
+
+        void inputText_Pasted(object sender, ClipboardEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                string[] lines = e.ClipboardText.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                int i = 0;
+                StringWriter text = new StringWriter();
+                foreach (var item in lines)
+                {
+                    text.WriteLine(++i + ": " + item);
+                }
+                inputText.Text = text.ToString(); ;
+            }
+            else
+            {
+                ((TextBox)sender).Text = e.ClipboardText;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoSearch();
+        }
+
+        private void DoSearch()
+        {
+            string[] splitstring = Regex.Split(inputText.Text, Environment.NewLine);
+            // Regex pattern = new Regex("Register(.*)=(.*)");
+            Regex pattern = null;
+            try
+            {
+                pattern = new Regex(inputSeach.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return;
+            }
+            
+            int lineCount = 0;
+
+            StringWriter text = new StringWriter();
+            foreach (string item in splitstring)
+            {
+                lineCount++;
+                Match match = pattern.Match(item);
+                //textBox2.Text += string.Format("{0} {1}",match.Groups[1],match.Groups[2].Value) + Environment.NewLine;
+
+
+                List<string> values = new List<string>();
+                for (int i = 1; i < match.Groups.Count; i++)
+                {
+                    values.Add(match.Groups[i].Value);
+                }
+                //textBox2.Text += string.Format("{0} \r\n", string.Join(" ", values.ToArray()));
+                try
+                {
+                    if (values.Count != 0)
+                    {
+                        if (checkBox1.Checked)
+                            text.WriteLine(string.Format(lineCount + ": " + outputString.Text, values.ToArray()));
+                        else
+                            text.WriteLine(string.Format(outputString.Text, values.ToArray()));
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+
+            }
+
+            outputText.Text = text.ToString();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DoSearch();
+        }
+    }
+    public class ClipboardEventArgs : EventArgs
+    {
+        public string ClipboardText { get; set; }
+        public ClipboardEventArgs(string clipboardText)
+        {
+            ClipboardText = clipboardText;
+        }
+    }
+
+    class MyTextBox : TextBox
+    {
+        public event EventHandler<ClipboardEventArgs> Pasted;
+
+        private const int WM_PASTE = 0x0302;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_PASTE)
+            {
+                var evt = Pasted;
+                if (evt != null)
+                {
+                    evt(this, new ClipboardEventArgs(Clipboard.GetText()));
+                }
+            }
+            else
+            {
+
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
